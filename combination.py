b4031e41498bb8c275a77b34b15c34fb969c7412
Faisal Wanth <heliconrngr@gmail.com>
2017-10-12 16:52:01 +0530

    added the required changes

--- combination.py
+++ combination.py
@@ -1,11 +1,12 @@
 #combination function in python
-def RecSubsets(soFar, rest)
-if rest==" "
-  print(soFar)
+def RecSubsets(soFar, rest):
+ print(soFar)
+ if rest ==" " :
+   print(soFar)
 
-  else :
+ else:
     RecSubsets(soFar+rest[0],rest[1:])
     RecSubsets(soFar, rest[1:])
 
 x =input()
-#RecSubsets("", x)
+RecSubsets(" ", x)
