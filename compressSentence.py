338ed5dabd1e1e4aa3098a099f8e83b713558e8f
Faisal Wanth <heliconrngr@gmail.com>
2017-10-12 21:19:30 +0530

    This is latest patch in the compressSentence

--- compressSentence.py
+++ compressSentence.py
@@ -1,20 +1,21 @@
 
 
-x= input()
+x= "the one bumble bee one bumble the bee"
 y=x.split()
 new_dict=dict()
+list = [element.upper() for element in y]
 
-for i in range(len(y)):
-    if y[i] in new_dict.keys():
+for i in range(len(list)):
+    if list[i] in new_dict.keys():
         print()
     else:
-        new_dict[y[i]]=i
+        new_dict[list[i]]=i
 
 print (new_dict)
 
 he=" "
 
 for j in range(len(y)):
-    he+=str(new_dict.get(y[j]))
+    he+=str(new_dict.get(list[j]))
 
 print(he)
