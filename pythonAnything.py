e32f39d7e79d5d09d46c54c20b30928be3ff95e0
Faisal Wanth <heliconrngr@gmail.com>
2017-10-16 11:12:00 +0530

    This is updated version of the class

---
+++ pythonAnything.py
@@ -0,0 +1,17 @@
+class SimpleProgram:
+
+  def __init__(self):
+   self.printHelloWorld()
+
+  def printHelloWorld(self):
+     print "HelloWorld/n"
+
+
+
+  if __name__ == "__main__":
+     run =SimpleProgram()
+
+     print("Program will execute")
+
+  else:
+      print("This class was imported and won't execute")
